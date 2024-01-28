7f6da1192faceb3380d9b9d9e7637f8ef50a21d0
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-28 07:35:18 +0530

    creating ec2

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ AMI=ami-0f3c7d07486cad139
 SG_ID=sg-04bb94f5d828fa09d
 ISTANCES=(mongodb, mysql, redis, rabbitmq, shipping, payment, user, cart, catalogue, dispatch, web)
 
-for i in ${INSTANCES[@]}
+for i in "${INSTANCES[@]}"
 do
     echo instance is: $i
     if [ i == "mongodb" ] || [ i == "shipping" ] || [ i == "mysql" ]
