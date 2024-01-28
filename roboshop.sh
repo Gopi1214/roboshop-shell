3d13c43816135931fdb16b3d07ec809c2167700a
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-28 07:34:27 +0530

    creating ec2

--- roboshop.sh
+++ roboshop.sh
@@ -4,8 +4,9 @@ AMI=ami-0f3c7d07486cad139
 SG_ID=sg-04bb94f5d828fa09d
 ISTANCES=(mongodb, mysql, redis, rabbitmq, shipping, payment, user, cart, catalogue, dispatch, web)
 
-for i in {$INSTANCES[@]}
+for i in ${INSTANCES[@]}
 do
+    echo instance is: $i
     if [ i == "mongodb" ] || [ i == "shipping" ] || [ i == "mysql" ]
     then
         INSTANCE_TYPE="t3.small"
