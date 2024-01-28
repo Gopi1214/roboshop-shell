cc871edd4faaeb8f213411f0db8233a904090ab8
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-28 07:30:26 +0530

    creating ec2

--- roboshop.sh
+++ roboshop.sh
@@ -8,9 +8,9 @@ for i in {$INSTANCES[@]}
 do
     if [ i == "mongodb" ] || [ i == "shipping" ] || [ i == "mysql" ]
     then
-        INSTANCE_TYPE = "t3.small"
+        INSTANCE_TYPE="t3.small"
     else
-        INSTANCE_TYPE = "t2.micro"
+        INSTANCE_TYPE="t2.micro"
     fi
 
     aws ec2 run-instances --image-id $AMI --count 1 --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID
