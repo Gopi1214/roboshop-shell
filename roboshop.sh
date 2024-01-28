4ef6df267d1b19d0a36324045d559ad56833f73f
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-28 08:54:53 +0530

    creating ec2

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ INSTANCES=(mongodb, mysql, redis, rabbitmq, shipping, payment, user, cart, catal
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is: $i"
+    #echo "instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "shipping" ] || [ $i == "mysql" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -15,6 +15,7 @@ do
     fi
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --count 1 --instance-type t2.micro  --security-group-ids sg-04bb94f5d828fa09d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress" --output text)
-    echo "instance is ip_address: $IP_ADDRESS"
+    #echo "instance is ip_address: $IP_ADDRESS"
+    echo "$i: $IP_ADDRESS"
 done
 
