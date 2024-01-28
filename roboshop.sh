b4c15a17186db84ee4891d059ee61a7a7b2c9cb0
Gopi1214 <jonnalagaddagopi12@gmail.com>
2024-01-28 09:20:00 +0530

    creating ec2

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@
 AMI=ami-0f3c7d07486cad139
 SG_ID=sg-04bb94f5d828fa09d
 INSTANCES=("mongodb" "mysql" "redis" "rabbitmq" "shipping" "payment" "user" "cart" "catalogue" "dispatch" "web")
+DOMAIN_NAME="pjdevops.online"
+ZONE_ID=Z08780431GOB4T1TR5RPR
 
 for i in "${INSTANCES[@]}"
 do
@@ -14,8 +16,28 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --count 1 --instance-type t2.micro  --security-group-ids sg-04bb94f5d828fa09d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress" --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress" --output text)
     #echo "instance is ip_address: $IP_ADDRESS"
     echo "$i: $IP_ADDRESS"
+
+#creating route53 records make sure delete old type A records
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch "
+    {
+    "Comment": "Testing creating a record set",
+    "Changes": [{
+    "Action"              : "CREATE",
+    "ResourceRecordSet"  : {
+        "Name"              : "$i.$DOMAIN_NAME"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "$IP_ADDRESS"
+        }]
+      }
+    }]
+  }
+  "
 done
 
